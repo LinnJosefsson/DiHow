085c2f01b6141bdb97777f3ddd558977da215e8b
San-Dan <70546259+San-Dan@users.noreply.github.com>
2021-03-10 19:34:11 +0100

    success msg after rsvp submit

---
+++ app.js
@@ -0,0 +1,26 @@
+// Confirm message after Booking
+
+const btnSubmit = document.querySelector('.f-btn');
+const form = document.querySelector('.rsvp-form');
+const fullName = document.querySelector('.f-name');
+const email = document.querySelector('.f-email');
+const msg1 = document.querySelector('#msg1');
+const msg2 = document.querySelector('#msg2');
+
+btnSubmit.addEventListener('click', () => {
+
+    if(fullName.value && email.value !== "") {
+        msg1.textContent = "Din plats är bokad!";
+        msg2.textContent = "Håll utkik i mailen efter en bekräftelse och mer information";
+        form.style.display ="none";
+         
+      }
+      if (fullName.value && email.value === "") {
+        false;
+        email.addEventListener("input", function (event) {
+          if (email.validity.typeMismatch) {
+            email.setCustomValidity("Ange en giltig email, tack!");
+          }
+        });
+      }
+    });
